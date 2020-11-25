0c54ea1ec9a05cc086408136c375cdc72c54ccc1
apparao@paktolus.com <apparo@paktolus.com>
2020-11-25 19:00:51 +0530

    angular routes added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {RouterModule,Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const appRoutes:Routes =[
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'request-password-reset',
+    component: RequestResetComponent
+  },
+  {
+    path: 'response-password-reset',
+    component: RequestResetComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
